0fd75f5d8d0723320a2c201e41c13393031d9926
Liam Gilhooly <liam.gilhooly@special-risks.co.uk>
2014-06-12 08:42:25 +0100

    structure of all pages altered.
    loads of changes made on the structure of each page.

--- processes/process_form.php
+++ processes/process_form.php
@@ -1,27 +1,31 @@
 <?php
+if( $_POST["submit"] ) 
+{					
+		
 $name = $_POST['name'];
 $email = $_POST['email'];
 $number = $_POST['number'];
 $message = $_POST['message'];
-
-if( !ereg('^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$', $email))
-	{	
+		
+	$valid = true;
+	if( !ereg('^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$', $email ) )
 	$valid = false;
-	}
-	
+
+	//if invalid send back to contact form with error flag
 	if( $valid == false ) 
 	{
-	header("Location:../contact_us.html?error=1" );
+		header( "Location:../oops.html" );
+		die( "invalid" );
 	}
 	else
 	{
-	
 	mail( "liam@special-risks.co.uk", "Trek4Care Contact Form Received", "From: $name\nEmail Address: $email\nPhone Number: $number\nMessage: $message", "FROM:webmaster@trek4care.co.uk" );
 		
 	mail( "vikburton@live.co.uk", "Trek4Care Contact Form Received", "From: $name\nEmail Address: $email\nPhone Number: $number\nMessage: $message", "FROM:webmaster@trek4care.co.uk" );
 	
-	mail( "$email", "Trek4Care Contact Form", "<p>Hi $email</p> <p>Thank you for getting in touch, we will get back to you just as soon as we are able to.</p>", "FROM:webmaster@trek4care.co.uk" );
+	mail( "$email", "Trek4Care Contact Form", "Hi $name, \n Thank you for getting in touch, we will get back to you just as soon as we are able to.", "FROM:webmaster@trek4care.co.uk" );
 
-	header( "Location:thankyou.html" );
+	header( "Location:../thankyou_form.html" );
 	}
+}	
 ?>
